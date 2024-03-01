04456a73badd5d4b29abe15584cea0e938f114cd
Krishnendu <krishnendu19802@gmail.com>
2024-03-01 22:32:16 +0530

    settings section changed

---
+++ src/Components/Settings/Modal.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+export default function Modal(props) {
+    console.log(props.username)
+    return (
+        <div>
+
+            {/* <button type="button" class="btn btn-primary" >
+                Launch static backdrop modal
+            </button> */}
+
+
+            <div class="modal fade" id="changeusername" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Change username</h1>
+                            
+                            {/* <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> */}
+                        </div>
+                        <div class="modal-body p-5">
+                            <div className="current-content d-flex">
+                                Current username: 
+                                <p className="bg-secondary p-2 text-light rounded ms-auto">{props.username}</p>
+                            </div>
+                            <div className="change-content d-flex ">
+                                New username:
+                                <input type="text" className="bg-secondary p-2 text-light ms-auto" />
+                            </div>
+                        </div>
+                        <div class="modal-footer d-flex justify-content-center">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" class="btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="modal fade" id="changeemail" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Change email</h1>
+                            
+                            {/* <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> */}
+                        </div>
+                        <div class="modal-body p-5">
+                            <div className="current-content d-flex">
+                                Current email: 
+                                <p className="bg-secondary p-2 text-light rounded ms-auto">{props.email}</p>
+                            </div>
+                            <div className="change-content d-flex ">
+                                New email:
+                                <input type="text" className="bg-secondary p-2 text-light ms-auto" />
+                            </div>
+                        </div>
+                        <div class="modal-footer d-flex justify-content-center">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal fade" id="changepassword" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Change Password</h1>
+                            
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body p-5">
+                            <div className="current-content d-flex">
+                                Current username: 
+                                <p className="bg-secondary p-2 text-light rounded ms-auto">{props.username}</p>
+                            </div>
+                            <div className="change-content d-flex ">
+                                New password:
+                                <input type="password" className="bg-secondary p-2 text-light ms-auto" />
+                            </div>
+                        </div>
+                        <div class="modal-footer d-flex justify-content-center">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideNavbar from '../SideNavbar/SideNavbar'
-
+import './Settings.css'
+import Modal from './Modal'
 export default function Settings() {
+  // const [change,setChange]=useState({'username':'User','email':'user@gmail','password':'********'})
+  const user={'username':'User','email':'user@gmail','password':'********'}
+  const handleClick=(e)=>{
+    // console.log(e.target.id)
+    // setChange((val)=>{
+    //   return {...change,tochange}
+    // })
+
+  }
   return (
     <div>
-      <SideNavbar/>
+      <Modal username={user.username} email={user.email}   />
+
+      <SideNavbar />
+      <div className={`settings-box  px-0 mt-1 d-flex align-items-center `}>
+        <div className="container-settings p-5  ">
+          <div className="user-settings rounded shadow p-2">
+            <div className="username container-fluid p-2 fs-4  align-items-center">
+              Current Username
+              <p className="rounded mx-2 fs-4 bg-secondary p-2 overflow-auto">User</p>
+              <button className="btn btn-danger btn-lg ms-auto" data-bs-toggle="modal" data-bs-target="#changeusername" id='username' onClick={handleClick}>Change</button>
+            </div>
+            <hr />
+            <div className="email  fs-4 p-2">
+              Current email
+              <p className="rounded bg-secondary p-2 mx-2 fs-4 overflow-auto">User@gmail.com</p>
+              <button className="btn btn-danger btn-lg ms-auto" data-bs-toggle="modal" data-bs-target="#changeemail" id='Email' onClick={handleClick}>Change</button>
+            </div>
+            <hr />
+            <div className="password  justify-content-center p-2">
+               <button className="btn btn-danger btn-lg " data-bs-toggle="modal" data-bs-target="#changepassword" id='password' onClick={handleClick}>Change Password</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
